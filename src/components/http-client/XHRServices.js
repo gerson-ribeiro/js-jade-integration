a2831deed90da71cc15532dd8fe6be2107cb5c6a
DXTCPS011079\gerson.paula <Gerson Ribeiro>
2021-09-01 15:44:40 -0300

    ...

--- src/components/http-client/XHRServices.js
+++ src/components/http-client/XHRServices.js
@@ -14,6 +14,7 @@ class XHRServices {
     }
 
     insertHeaders() {
+        if (this._header) return;
         for (let i = 0; i < this.header.length; i++) {
             const head = this.header[i];
 
